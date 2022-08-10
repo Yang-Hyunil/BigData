e239925adb31c087d0d3dd376f7ec4175180828a
OSS <OSS@DESKTOP-7PSM2HP>
2022-08-10 17:20:44 +0900

    집에 갑시다.

---
+++ BigData/src/class301/Oss.java
@@ -0,0 +1,12 @@
+package class301;
+
+public class Oss implements Print {
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("집에 갑시다. 모두들 수고하셨습니다.");
+
+	}
+
+}
