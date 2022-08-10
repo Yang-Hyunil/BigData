6148a4b9da9c7da92cedba11042e771702f0b6d6
lnu <lnu@DESKTOP-EQ0JANR>
2022-08-10 17:33:36 +0900

    맞습니까 맞아요? 이거?>

--- BigData/src/class301/Lnw.java
+++ BigData/src/class301/Lnw.java
@@ -5,7 +5,9 @@ public class Lnw implements Print {
 	@Override
 	public void print() {
 		System.out.println("귯걸~");
+		System.out.println("신기합니다.");
 
+		System.out.println("왜 내꺼 안 올라가!");
 	}
 
 }
