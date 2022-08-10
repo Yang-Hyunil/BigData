df37d2c1a3490fee5dd2ef127f70192190f50df1
OSS <OSS@DESKTOP-7PSM2HP>
2022-08-10 17:26:39 +0900

    수정버전입니다
    오늘 하루도 고생하셨습니다.

--- BigData/src/class301/Oss.java
+++ BigData/src/class301/Oss.java
@@ -5,7 +5,8 @@ public class Oss implements Print {
 	@Override
 	public void print() {
 		// TODO Auto-generated method stub
-		System.out.println("집에 갑시다. 모두들 수고하셨습니다.");
+		System.out.println("집에 갑시다. 모두들 수고하셨습니다!!!.");
+		System.out.println("이틀만 더 나오면 삼일 휴가입니다.");
 
 	}
 
