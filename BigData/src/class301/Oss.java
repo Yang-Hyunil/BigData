325c82ea61e17cbdf28b1119816c5dd6bd981faa
OSS <OSS@DESKTOP-7PSM2HP>
2022-08-10 17:31:33 +0900

    수정버전입니다.
    집에 갑시다.

--- BigData/src/class301/Oss.java
+++ BigData/src/class301/Oss.java
@@ -7,7 +7,7 @@ public class Oss implements Print {
 		// TODO Auto-generated method stub
 		System.out.println("집에 갑시다. 모두들 수고하셨습니다!!!.");
 		System.out.println("이틀만 더 나오면 삼일 휴가입니다.");
-		System.out.println("아싸! 20분 후면 집에가네요 ");
+		System.out.println("아싸! 20분 후면 집에가네요!!!!!! ");
 
 	}
 
