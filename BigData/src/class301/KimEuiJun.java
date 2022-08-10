957db757a952ddced360af6583087dbc38ee2bd4
KUJ <KUJ@DESKTOP-HNO866N>
2022-08-10 17:27:56 +0900

    김의준 4번째

--- BigData/src/class301/KimEuiJun.java
+++ BigData/src/class301/KimEuiJun.java
@@ -6,7 +6,7 @@ public class KimEuiJun implements Print {
 	public void print() {
 		// TODO Auto-generated method stub
 		System.out.println("되는건가 !");
-		System.out.println("언제가능 !?");
+		System.out.println("나도 하고싶다.");
 	}
 
 }
